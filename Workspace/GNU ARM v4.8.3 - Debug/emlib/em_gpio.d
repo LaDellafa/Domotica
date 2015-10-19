24226fdc85ff8a410d4e6ef6c80d3596037a93ae
jensdehoog95 <jensdehoog@icloud.com>
2015-10-19 16:11:44 +0200

    Workspace toegevoegd

---
+++ Workspace/GNU ARM v4.8.3 - Debug/emlib/em_gpio.d
@@ -0,0 +1,2 @@
+emlib/em_gpio.o: \
+ /Applications/SimplicityStudio_v3/developer/sdks/efm32/v2/emlib/src/em_gpio.c
---
+++ Workspace/src/Relais.h
@@ -0,0 +1,13 @@
+/*
+ * Bediening.h
+ *
+ *  Created on: 18 okt. 2015
+ *      Author: jensdehoog
+ */
+
+#ifndef BEDIENING_H_
+#define BEDIENING_H_
+
+void Relais_bediening(int status);
+
+#endif /* BEDIENING_H_ */
